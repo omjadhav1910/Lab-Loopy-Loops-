0b605a7e6d807b8176afcddd9d3386a08ae72bf9
Om Jadhav <144478519+omjadhav1910@users.noreply.github.com>
2023-10-30 11:46:34 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@
 // 1.2 Print `"The driver's name is XXXX"`.
 // 1.3 Create a variable `Kalvian-2` with the navigator's name.
 // 1.4 Print `"The navigator's name is YYYY"`.
-let kalvian1 = 'Kalvian1';
+let kalvian1 = 'om';
 console.log("The driver's name is " + kalvian1);
 
 let kalvian2 = 'kalvian2';
@@ -100,20 +100,50 @@ if (kalvian1.localeCompare(kalvian2) < 0) {
 
 // Bonus Time!
 // Bonus 1:
+
 // Go to lorem ipsum generator and:
 // Generate 3 paragraphs. Store the text in a variable type of string.
 // Make your program count the number of words in the string.
 // Make your program count the number of times the Latin word et appears.
+
+const loremIpsum = `
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed feugiat libero at ligula congue, vel suscipit ligula lacinia. Integer a accumsan neque. Etiam eu dui eget justo dictum sollicitudin. Suspendisse vel bibendum justo. Nulla facilisi. In hac habitasse platea dictumst. Etiam fermentum tristique vehicula. Suspendisse potenti. Sed egestas tellus nec rhoncus. Vivamus nec odio dolor. Sed venenatis in justo et finibus. Etiam sed bibendum tortor. Curabitur tincidunt purus a dui viverra luctus.
+`;
+
+function countWords(text) {
+  // Split the text into words using whitespace as a delimiter.
+  const words = text.split(/\s+/);
+  return words.length;
+}
+
+const Countword = countWords(loremIpsum);
+console.log(`Word count in the lorem ipsum text: ${Countword} words`);
+
+function LatinWord(text, word) {
+  const regex = new RegExp(`\\b${word}\\b`, 'g');
+  const matches = text.match(regex);
+  return matches ? matches.length : 0;
+}
+
+const latinWordCount = LatinWord(loremIpsum, 'et');
+console.log(
+  `Count of the Latin word "et" in the lorem ipsum text: ${latinWordCount} times`
+);
+
 // Bonus 2:
-// Create a new variable phraseToCheck and have it contain some string value. Write a code that will check if the value we assigned to this variable is a Palindrome. Here are some examples of palindromes:
 
-// "A man, a plan, a canal, Panama!"
-// "Amor, Roma"
-// "race car"
-// "stack cats"
-// "step on no pets"
-// "taco cat"
-// "put it up"
-// "Was it a car or a cat I saw?" and "No 'x' in Nixon".
+// Create a new variable phraseToCheck and have it contain some string value.
 
-// Hint: If you use Google to help you to find solution to this iteration, you might run into some solutions that use advanced string or array methods (such as join(), reverse(), etc.). However, try to apply the knowledge you currently have since you can build pretty nice solution with just using for loop, if-else statements with some break and continue... Just sayin'
+const phraseToCheck = 'A man, a plan, a canal, Panama!';
+
+function isPalindrome(phrase) {
+  const cleanedPhrase = phrase.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+  const reversedPhrase = cleanedPhrase.split('').reverse().join('');
+  return cleanedPhrase === reversedPhrase;
+}
+
+if (isPalindrome(phraseToCheck)) {
+  console.log(`"${phraseToCheck}" is a palindrome.`);
+} else {
+  console.log(`"${phraseToCheck}" is not a palindrome.`);
+}
